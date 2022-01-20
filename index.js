db458f402d6397760181cb00f5ab2bf1f949c17b
elidakirigo <elidakirigo@gmail.com>
2022-01-20 20:29:41 +0300

    ,

--- index.js
+++ index.js
@@ -11,11 +11,16 @@ const simpleGit = require("simple-git");
 const random = require("random");
 const FILE_PATH = "./data.json";
 
-const makeCommit = n => {
+const makeCommit = (n) => {
   if (n === 0) return simpleGit().push();
   const x = random.int(0, 54);
   const y = random.int(0, 6);
-  const DATE = moment().subtract(1, "y").add(1, "d") .add(x, "w").add(y, "d").format();
+  const DATE = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format();
   const data = { date: DATE };
   console.log(DATE);
 
