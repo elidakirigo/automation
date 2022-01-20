160aab25be465ae47130025babe5f43c873d9ca7
elidakirigo <elidakirigo@gmail.com>
2022-01-20 20:22:02 +0300

    .

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
+/*----------------------------------------------------------------------------------------------------------------
+* this file uses the following npm packages 
+* 1. moment.js
+* 2. simple-git.js
+* 3. random.js
+----------------------------------------------------------------------------------------------------------------*/
+
 const jsonfile = require("jsonfile");
 const moment = require("moment");
 const simpleGit = require("simple-git");
 const random = require("random");
 const FILE_PATH = "./data.json";
 
-const makeCommit = (n) => {
+const makeCommit = n => {
   if (n === 0) return simpleGit().push();
   const x = random.int(0, 54);
   const y = random.int(0, 6);
-  const DATE = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
+  const DATE = moment().subtract(1, "y").add(1, "d") .add(x, "w").add(y, "d").format();
   const data = { date: DATE };
   console.log(DATE);
 
@@ -25,4 +27,3 @@ const makeCommit = (n) => {
   });
 };
 makeCommit(200);
-
