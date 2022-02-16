2ac01af5a7c99c16ada9d3e68ccd8dc5ed07bb39
elidakirigo <elidakirigo@gmail.com>
2022-02-16 15:27:00 +0300

    j

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ const FILE_PATH = "./data.json";
 
 const makeCommit = (n) => {
   if (n === 0) return simpleGit().push();
-  const x = random.int(0, 54);
+  const x = random.int(20, 40);
   const y = random.int(0, 6);
+  // const x = 4;
+  // const y =4;
   const DATE = moment()
     .subtract(1, "y")
     .add(1, "d")
@@ -31,4 +33,4 @@ const makeCommit = (n) => {
       .push();
   });
 };
-makeCommit(200);
+makeCommit(100);
