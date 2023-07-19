a69ba02f8a3aa9cf09517fb92aee164045617ccf
el <elidakirigo@gmail.com>
2023-07-19 22:48:03 +0300

    `

--- src/action.js
+++ src/action.js
@@ -6,6 +6,11 @@ async function run() {
 	const octokit = github.getOctokit(GITHUB_TOKEN)
 	const { context = {} } = github
 	const { pull_request } = context.payload
+	await octokit.issues.createComment({
+		...context.repo,
+		issue_number:pull_request.number,
+		body:'thankyou for submitting a pull request! we will try to reach out as soon as possible'
+	})
 	console.log('hello world! github'); 
 }
 run()
