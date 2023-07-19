8916d61b8d8ba40f201435af439bd1dfcb48d82b
el <elidakirigo@gmail.com>
2023-07-19 22:23:12 +0300

    pull test

--- src/action.js
+++ src/action.js
@@ -2,7 +2,10 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 async function run() {
-	
-	console.log('hello world! github');
+	const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
+	const octokit = github.getOctokit(GITHUB_TOKEN)
+	const { context = {} } = github
+	const { pull_request } = context.payload
+	console.log('hello world! github'); 
 }
 run()
